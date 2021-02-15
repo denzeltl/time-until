25b56c2fd1e3aeb8a684c1ec014b5793c2226c50
Denzel <denzeltlee@gmail.com>
2021-02-15 18:13:55 +0800

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    main: {},
+    headerTitle: {
+        background: theme.palette.primary.main,
+        borderBottom: `0.5rem solid ${theme.palette.secondary.main}`,
+        textAlign: "center",
+        padding: "2rem 0",
+    },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <main className={classes.main}>
+                <Typography variant="h3" component="h1" className={classes.headerTitle}>
+                    Create React App
+                </Typography>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#267fa6",
+        },
+        secondary: {
+            main: "#eb7628",
+        },
+        success: {
+            main: "#42b347",
+        },
+        error: {
+            main: "#ef4a4a",
+        },
+        background: {
+            default: "#ffffff",
+        },
+    },
+    typography: {
+        fontFamily: ["Lato", "Roboto", "Helvetica", "Arial", "sans-serif"].join(","),
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 480,
+            md: 768,
+            lg: 992,
+            xl: 1200,
+        },
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
